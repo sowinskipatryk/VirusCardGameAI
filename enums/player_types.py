c606d32879ceb54a227eba2c6d814bc27f8005fd
sowinskipatryk <sowinski.patryk@wp.pl>
2025-04-26 23:45:00 +0200

    updated players

--- enums/player_types.py
+++ enums/player_types.py
@@ -4,5 +4,5 @@ from enum import StrEnum
 class PlayerType(StrEnum):
     HUMAN = "Human"
     RANDOM = "Random"
-    EXPLORER = "Explorer"
-    RULE_BASED = "RuleBased"
+    NEAT_AI = "NEAT_AI"
+    RULE_BASED_AI = "RuleBasedAI"
